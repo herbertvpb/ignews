58590cdd4fbc4368a834b7f019d7b479c496ab08
Herbert Vieira <herbert.vieira@clubpetro.com>
2021-07-02 17:19:41 -0300

    implements github authentication through next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,25 +1,27 @@
-import { FaGithub as GithubIcon } from 'react-icons/fa';
-import { FiX as CloseIcon } from 'react-icons/fi';
+import { FaGithub as GithubIcon } from 'react-icons/fa'
+import { FiX as CloseIcon } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
-
 export default function SignInButton() {
-  const isLoggedIn = true;
+  const [session] = useSession()
 
-  return isLoggedIn ? (
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <GithubIcon color="#04d361" />
-      Herbert Vieira
+      {session.user.name}
       <CloseIcon color="#737380" className={styles.closeIcon} />
     </button>
   ) :  (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <GithubIcon color="#eba417" />
       Sign in with Github
